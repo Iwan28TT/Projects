5448a9dc5ae739b748e6a5a82810bfd85a1ed635
Iwan28TT <117277096+Iwan28TT@users.noreply.github.com>
2023-04-18 11:03:27 +0200

    Add files via upload

---
+++ login-system-blog/login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+<?php
+    include('db.php');
+    session_start();
+    // When form submitted, check and create user session.
+    if (isset($_POST['username'])) {
+        $username = stripslashes($_REQUEST['username']);    // removes backslashes
+        $username = mysqli_real_escape_string($conn, $username);
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($conn, $password);
+        // Check user is exist in the database
+        $query    = "SELECT * FROM `users` WHERE username='$username'
+                     AND password='" . md5($password) . "'";
+        $result = mysqli_query($conn, $query) or die(mysql_error());
+        $rows = mysqli_num_rows($result);
+        if ($rows == 1) {
+            $res = $result ->fetch_assoc();
+            $_SESSION['username'] = $username;
+            $_SESSION['profielFoto'] = $res['profielFoto'];
+            $_SESSION['userid'] = $res['id'];
+            // Redirect to user dashboard page
+            header("Location: dashboard.php");
+        } else {
+            echo "<div class='form'>
+                  <h3>Incorrect Username/password.</h3><br/>
+                  <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
+                  </div>";
+        }
+    } else {
+?>
+    <form class="form" method="post" name="login">
+        <h1 class="login-title">Login</h1>
+        <input type="text" class="login-input" name="username" placeholder="Gebruikersnaam" autofocus="true"/>
+        <input type="password" class="login-input" name="password" placeholder="Wachtwoord"/>
+        <input type="submit" value="Login" name="submit" class="login-button"/>
+        <p class="link">Heb je nog geen account? <a href="registration.php">Registreer hier!</a></p>
+  </form>
+<?php
+    }
+?>
+</body>
+</html>
